c660980496fd4ceeb952462984bc6e0b31f1a15f
Jskonst <jskonst@yandex.ru>
2017-01-19 02:05:50 +0300

    added to production

--- WEB-INF/classes/facade.js
+++ WEB-INF/classes/facade.js
@@ -109,7 +109,7 @@
             });
             break;
         default:
-            throw 'Unknown application type at runtime.';
+            throw 'Unknown application type in runtime.';
     }
 }(function () {
     var module = {};
